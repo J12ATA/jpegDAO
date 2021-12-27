f28f29945e5855f26e2886fee0de57550ca1cc46
Jose Figueroa <contact@josefig.com>
2021-12-26 21:28:02 -0800

    added mint and member page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 import { useEffect, useMemo, useState } from 'react';
-
-// import thirdweb
 import { useWeb3 } from '@3rdweb/hooks';
+import { ThirdwebSDK } from '@3rdweb/sdk';
+
+const sdk = new ThirdwebSDK('rinkeby');
+const contractAddress = '0x4dc9940c00Ff567469B77dbE99988e010Fe663E0';
+const bundleDropModule = sdk.getBundleDropModule(contractAddress);
 
 const App = () => {
-  // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
   console.log('👋 Address:', address);
 
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
+  // The signer is required to sign transactions on the blockchain.
+  // Without it we can only read data, not write.
+  const signer = provider ? provider.getSigner() : undefined;
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // isClaiming lets us easily keep a loading state while the NFT is minting.
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  // Another useEffect!
+  useEffect(() => {
+    // We pass the signer to the sdk, which enables us to interact with
+    // our deployed contract!
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    return bundleDropModule
+      .balanceOf(address, '0')
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log('🌟 this user has a membership NFT!');
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error('failed to nft balance', error);
+      });
+  }, [address]);
+
   if (!address) {
     return (
       <div className="landing">
@@ -21,11 +57,43 @@ const App = () => {
     );
   }
 
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🖼️DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  }
+
+  const mintNft = () => {
+    setIsClaiming(true);
+    // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
+    bundleDropModule
+      .claim('0', 1)
+      .catch((err) => {
+        console.error('failed to claim', err);
+        setIsClaiming(false);
+      })
+      .finally(() => {
+        // Stop loading state.
+        setIsClaiming(false);
+        // Set claim state.
+        setHasClaimedNFT(true);
+        // Show user their fancy new NFT!
+        console.log(
+          `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`
+        );
+      });
+  };
+
+  // Render mint nft screen.
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
+    <div className="mint-nft">
+      <h1>Mint your free 🖼️DAO Membership NFT</h1>
+      <button disabled={isClaiming} onClick={() => mintNft()}>
+        {isClaiming ? 'Minting...' : 'Mint your nft (FREE)'}
+      </button>
     </div>
   );
 };
