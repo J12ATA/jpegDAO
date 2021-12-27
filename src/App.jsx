3b89d7becb186572580ae6b831076efeb891cad3
Jose Figueroa <contact@josefig.com>
2021-12-26 22:06:59 -0800

    display members with holdings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useWeb3 } from '@3rdweb/hooks';
 import { ThirdwebSDK } from '@3rdweb/sdk';
+import { ethers } from 'ethers';
 
 const sdk = new ThirdwebSDK('rinkeby');
 const contractAddress = '0x4dc9940c00Ff567469B77dbE99988e010Fe663E0';
 const bundleDropModule = sdk.getBundleDropModule(contractAddress);
+const tokenAddress = '0x23a701A5Fb89b13528C48d0bd305C4f7e0d8b05d';
+const tokenModule = sdk.getTokenModule(tokenAddress);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
@@ -17,6 +20,68 @@ const App = () => {
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
+
+  // Holds the amount of token each member has in state.
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  // The array holding all of our members addresses.
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  // A fancy function to shorten someones wallet address, no need to show the whole thing.
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + '...' + str.substring(str.length - 4);
+  };
+
+  // This useEffect grabs all our the addresses of our members holding our NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    bundleDropModule
+      .getAllClaimerAddresses('0')
+      .then((addresess) => {
+        console.log('🚀 Members addresses', addresess);
+        setMemberAddresses(addresess);
+      })
+      .catch((error) => {
+        console.error('failed to get member list', error);
+      });
+  }, [hasClaimedNFT]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log('👜 Amounts', amounts);
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((error) => {
+        console.error('failed to get token amounts', error);
+      });
+  }, [hasClaimedNFT]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   // Another useEffect!
   useEffect(() => {
@@ -57,11 +122,36 @@ const App = () => {
     );
   }
 
+  // If the user has already claimed their NFT we want to display the interal DAO page to them
+  // only DAO members will see this. Render all the members + token amounts.
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
         <h1>🖼️DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
@@ -71,8 +161,8 @@ const App = () => {
     // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
     bundleDropModule
       .claim('0', 1)
-      .catch((err) => {
-        console.error('failed to claim', err);
+      .catch((error) => {
+        console.error('failed to claim', error);
         setIsClaiming(false);
       })
       .finally(() => {
